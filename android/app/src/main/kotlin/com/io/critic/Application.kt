a9e4c3adfb12f28291503cb82f3fed9c3657bc53
Trey Hope <trey.a.hope@gmail.com>
2021-04-25 10:46:51 -0400

    Fixed bug in application.kt.

--- android/app/src/main/kotlin/com/io/critic/Application.kt
+++ android/app/src/main/kotlin/com/io/critic/Application.kt
@@ -1,19 +1,13 @@
-package com.io.critic
-
 import io.flutter.app.FlutterApplication
 import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
 
-class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
-    override fun onCreate() {
-        super.onCreate()
-        FlutterFirebaseMessagingService.setPluginRegistrant(this);
-        // ...
-    }
+import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingPlugin
 
-    override fun registerWith(registry: PluginRegistry?) {
-        FirebaseMessagingPlugin.registerWith(registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"))
-        // ...
+class Application() : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
+  override fun registerWith(registry: PluginRegistry?) {
+      val key: String? = FlutterFirebaseMessagingPlugin::class.java.canonicalName
+      if (!registry?.hasPlugin(key)!!) {
+          FlutterFirebaseMessagingPlugin.registerWith(registry?.registrarFor("io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingPlugin"));
+        }
     }
 }
